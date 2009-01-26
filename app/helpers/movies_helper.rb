6a8ee566d5637e7eac76a8199020e93971096ad0
unknown <Becca@.(none)>
2009-01-25 19:28:58 -0500

    Added genre to movie

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,7 @@
 module MoviesHelper
+
+  def genre_select_options
+    Genre.find(:all).collect {|p| [ p.name, p.id ] } .unshift(["N/A", nil])
+  end
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ActiveRecord::Base
+  belongs_to :genre
+
+  def genre_name
+
+    if genre.nil?
+      return "N/A"
+    else
+      return genre.name
+    end
+  end
 end
