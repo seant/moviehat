5c9b7159c0be674733d54a8dd3b24fa5c19bbcd1
unknown <dean@stautberg.net>
2009-02-12 00:00:56 -0500

    added some movie unit tests

--- test/unit/movie_test.rb
+++ test/unit/movie_test.rb
@@ -2,7 +2,16 @@ require 'test_helper'
 
 class MovieTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  def setup
+    Movie.new(:title=>'aaaaaaa', :description=>'saasdfasdfas').save!
+    Movie.new(:title=>'bbbbbbb', :description=>'qwerqwerqwerqwerfas').save!
+  end
+
+  test "text search on movies" do
+    assert_equal 1, Movie.find_by_search_text('aa').size
+    assert_equal 1, Movie.find_by_search_text('bbb').size
+    assert_equal 2, Movie.find_by_search_text('fas').size
+    assert_equal 0, Movie.find_by_search_text('dean is awesome').size
   end
 end
