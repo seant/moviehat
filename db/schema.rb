02a6dc2d35361fa1f2a51b5531db622c3a50a639
unknown <sean.thornton@gmail.com>
2009-02-22 14:45:56 -0500

    Added base config/objects for restful_authentication.  Not fully working but shouldn't affect existing functionality.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090125201526) do
+ActiveRecord::Schema.define(:version => 20090222192209) do
 
   create_table "genres", :force => true do |t|
     t.string   "name"
@@ -26,5 +26,23 @@ ActiveRecord::Schema.define(:version => 20090125201526) do
     t.datetime "updated_at"
     t.integer  "genre_id"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "login",                     :limit => 40
+    t.string   "name",                      :limit => 100, :default => ""
+    t.string   "email",                     :limit => 100
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_token",            :limit => 40
+    t.datetime "remember_token_expires_at"
+    t.string   "activation_code",           :limit => 40
+    t.datetime "activated_at"
+    t.string   "state",                                    :default => "passive"
+    t.datetime "deleted_at"
+  end
+
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
 
 end
